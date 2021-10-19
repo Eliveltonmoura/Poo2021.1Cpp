a03e2b17fbf06e4860f43094a47600c09923823c
Elivelton Moura <eliveltonmoura112@gmail.com>
2021-10-19 12:36:43 -0300

    Teste02

--- primeiro.cpp
+++ primeiro.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main(){
          cout << "-------------------";
-         cout << "Boa noite teste \n";
+         cout << "Boa noite teste ";
          cout << "-------------------";
 
 }
