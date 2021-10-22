07ef9af7069a7c65382f613fc753bef49ff87f90
Elivelton Moura <eliveltonmoura112@gmail.com>
2021-10-21 21:13:44 -0300

    Boa noite

--- Lista1/main.cpp
+++ Lista1/main.cpp
@@ -1,6 +1,8 @@
+//Biblioteca
 #include <iostream>
 #include <string> 
  using namespace std;
+ //classe usuario
 class Usuario{
 private:
      string nome;
@@ -9,10 +11,11 @@ private:
      string palavraChave;
      int idade;
      char sexo;  
-public:    
+public: 
+    //construtor   
     Usuario( string nome, string ocupacao, string email, string palavraChave, int idade, char sx );
         
-    // sets
+    // setes
     void setNome (string nome){
          this->nome = nome;
     }
@@ -31,7 +34,7 @@ public:
      char setSexo (char sexo ){
             sexo = sexo;
     }
-    //gets
+    //getes
     string getNome (){
         return nome;
     }
@@ -53,6 +56,7 @@ public:
     // fução de imprimir
     void imprimir(int num);
 };
+ //construtor
  Usuario::Usuario( string nome, string ocupacao, string email, string palavraChave, int idade, char sx ){
         this->nome = nome;
         this->ocupacao = ocupacao;
@@ -62,7 +66,7 @@ public:
                sexo = sx;            
        
     }
-
+ // fução de imprimer os resutado consutados
  void Usuario::imprimir( int num){
     int op=num;
     if (op==1){
@@ -98,19 +102,26 @@ public:
 
     
  }
-
+ // função principal
 int main (){ 
-     
-    Usuario *professor = new Usuario("bruna","professor","bruna@gmail.com","prof",18,'M');
-    Usuario *tecnico = new Usuario("maria","tec","maria@gmail.com","tec",18,'M');
-    Usuario *aluno = new Usuario("Alicia","estutante","alicia@gmail.com","alu",18,'M');
-    // cout >> professor->getNome;
-    
-   // Usuario p();       
-     
+      /*      
+      1.Professor 
+      2.técnico
+      3.Aluno      
+      */
+
+
+
+     // inicialização do objeto 
+    Usuario *professor = new Usuario("bruna","professor","bruna@gmail.com","prof",18,'F');
+    Usuario *tecnico = new Usuario("Elivelton","tecnico","velton@alu.ufc.br","tec",25,'M');
+    Usuario *aluno = new Usuario("Alicia","estutante","alicia@gmail.com","alu",18,'F');
+         
     professor->imprimir(1);
-    //tecnico.imprimir(2);
-    //aluno.imprimir(3);
+
+    tecnico->imprimir(2);
+    
+    aluno->imprimir(3);
 
     
     return 0;
