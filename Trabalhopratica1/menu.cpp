6a39fe4a0095311a077b6ed58d315ca61ab15fd6
Elivelton Moura <78855380+Eliveltonmoura@users.noreply.github.com>
2022-02-01 13:27:46 -0300

    add

--- Trabalhopratica1/menu.cpp
+++ Trabalhopratica1/menu.cpp
@@ -1,10 +1,17 @@
 #include "menu.h" // chamada da intefase menu;
 #include "Cd.h"   // chamada da intefase Cd;
+#include <string>
 
 void menu()
 {
     bool fasa = true;
     int op;
+    string art = {""};
+    string tit = {""};
+    int ano = {0};
+    string gen = {""};
+    int dur = {0};
+    float vol = {0.0};
     // criação do objeto Cd
     Cd n = Cd("Elivelton", "Um Amor", 2008, "Romance", 3, 2.5, true);
 
@@ -14,8 +21,9 @@ void menu()
              << "1.Busca\n"
              << "2.Mostra Faixas\n"
              << "3.Cadastra \n"
+             << "0.Sair \n"
              << "\n";
-        cout << " Digite uma opcao: ";
+        cout << "Digite uma opcao: ";
         cin >> op;
         switch (op)
         {
@@ -26,10 +34,35 @@ void menu()
         case 2:
             cout << "-------------------------Faixas--------------------------\n";
             n.print();
-
             break;
+        case 3:
+
+            cout << "Cadastrar Faixa  \n";
+            cout << "Artista: ";
+            cin >> art;
+            cout << "Titulo: ";
+            cin >> tit;
+            cout << "Ano de lansamento: ";
+            cin >> ano;
+            cout << "Genero: ";
+            cin >> gen;
+            cout << "Duração: ";
+            cin >> dur;
+            cout << "Volune: ";
+            cin >> vol;
+            // setando os valores nos atributos
+            n.setArtista(art);
+            n.setTitulo(tit);
+            n.setLansamento(ano);
+            n.setGenero(gen);
+            n.setDuracao(dur);
+            n.setVolume(vol);
+            break;
+        case 0:
+            fasa = false;
 
         default:
+
             break;
         }
 
