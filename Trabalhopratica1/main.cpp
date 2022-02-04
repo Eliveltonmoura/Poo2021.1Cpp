4d27e18c8b285b92f4a2b98b9442998bd9368cc8
Elivelton Moura <78855380+Eliveltonmoura@users.noreply.github.com>
2022-02-03 21:06:57 -0300

    add

--- Trabalhopratica1/main.cpp
+++ Trabalhopratica1/main.cpp
@@ -1,35 +1,36 @@
 #include <iostream>
-#include "DrivesFucao.h"
-#include "Midia.h"
+#include <algorithm>
+#include <map>
+#include <iterator>
+#include <utility>
+#include <string>
+#include <vector>
 #include "Cd.h"
 #include "Dvd.h"
 using namespace std;
+bool compara_lancamento(Midia i, Midia j)
+{
+    return i.getLansamento() > j.getLansamento();
+}
+vector<Cd> artistas(vector<Cd> cd, string artista)
+{
+    vector<Cd> resposta;
+
+    for (Cd i : cd)
+    {
+        if (i.getArtista() == artista)
+        {
+            resposta.push_back(i);
+        }
+    }
+    sort(resposta.begin(), resposta.end(), compara_lancamento);
+    return resposta;
+}
 
 int main()
 {
 
-    /// clamada da função menu
-    // menu();
-
-    /*
-     bool compara_lancamento(Midia i, Midia j)
-     {
-         return i.getLansamento() > j.getLansamento();
-
-
-    vector<Cd> artistas(vector<Cd> cd, string artista)
-    {
-        vector<Cd> resposta;
-
-        for (Cd i : cd)
-        {
-            if (i.getArtista() == artista)
-            {
-                resposta.push_back(i);
-            }
-        }
-        /*sort(resposta.begin(), resposta.end(), compara_lancamento);
-        return resposta;*/
+    // vector<Cd> c = Cd("Artista", "titulo", "faixa", 2222, "genero", "000", 2, 3.3);
 
     return 0;
 }
